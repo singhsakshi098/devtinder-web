9153e08be128317bf70d7a2c48f92c931673eede
sakshi <sakshi0981456@gmail.com>
2026-06-21 20:15:53 +0530

    build useCard

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,11 +1,42 @@
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch , useSelector } from "react-redux";
+import { addFeed } from "../utils/feedSlice";
+import { useEffect } from "react";
+import  UserCard from "./UserCard";
+
 
 const Feed = () => {
+  const feed = useSelector((store) => store.feed);
+  console.log(feed);
+  const dispatch = useDispatch()
+
+  const getFeed = async () => {
+    if(feed) return;
+    
+    try{const res = await axios.get(BASE_URL + "/user/feed" , 
+      {withCredentials: true});
+    dispatch(addFeed(res.data));}
+    catch (err) {
+      //to do error
+       console.log(err);
+  console.log(err.response?.status);
+  console.log(err.response?.data);
+    }
+
+  };
+
+  useEffect(() => {
+    getFeed();
+  }, []);
   return (
-    <div>
-        feed
+    feed &&  (
+    <div className ="flex justify-center my-10">
+        <UserCard user={feed[0]} />
       
     </div>
-  )
-}
+    )
+  );
+};
 
 export default Feed
---
+++ src/components/UserCard.jsx
@@ -0,0 +1,45 @@
+const UserCard = ({ user }) => {
+  const { firstName, lastName, photoUrl, age, gender, about } = user;
+
+  return (
+    <div className="card w-96 bg-base-200 shadow-2xl rounded-2xl border border-base-300">
+      <figure className="h-96">
+        <img
+          src={photoUrl || "https://randomuser.me/api/portraits/women/44.jpg"}
+          alt="Profile"
+          className="w-full h-full object-cover"
+        />
+      </figure>
+
+      <div className="card-body">
+        <h2 className="card-title text-3xl font-bold">
+          {firstName} {lastName}
+        </h2>
+
+        {age && gender && (
+          <p className="text-sm opacity-70">
+            {age} • {gender}
+          </p>
+        )}
+
+        <p className="mt-2 text-base leading-6">
+          {about}
+        </p>
+
+        <div className="divider my-2"></div>
+
+        <div className="card-actions justify-between">
+          <button className="btn btn-error w-32">
+            Ignore
+          </button>
+
+          <button className="btn btn-primary w-32">
+            Interested
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserCard;
